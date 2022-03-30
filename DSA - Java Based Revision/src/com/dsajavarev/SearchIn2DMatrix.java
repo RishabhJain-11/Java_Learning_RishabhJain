957e838a5c0f7f91c55d691988f15c3b5de8483e
Rishabh Jain <jainrishabh71011@gmail.com>
2022-03-30 17:29:31 +0530

    Added on 28th March 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/SearchIn2DMatrix.java
@@ -0,0 +1,22 @@
+package com.dsajavarev;
+
+public class SearchIn2DMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        int target = 13;
+        System.out.println(searchMatrix(matrix,target));
+    }
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0,j = matrix[0].length - 1;
+        while(i < matrix.length && j >= 0){
+            if(matrix[i][j] == target){
+                return true;
+            }else if(matrix[i][j] > target){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return false;
+    }
+}
