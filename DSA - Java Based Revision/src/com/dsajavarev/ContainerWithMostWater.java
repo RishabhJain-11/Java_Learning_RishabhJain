27a29f39b5129a695828751fd79554ead2912dfa
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-05 15:43:04 +0530

    Added on 05th April 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+package com.dsajavarev;
+
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height));
+    }
+    public static int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            maxArea = Math.max(maxArea, Math.min(height[left], height[right])
+                    * (right - left));
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return maxArea;
+    }
+}
