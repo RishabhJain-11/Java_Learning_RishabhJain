f16dcdb4e6199250e4f723ce989912562c28310a
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-10 13:18:03 +0530

    Added on 10th April 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/SecondMostString.java
@@ -0,0 +1,39 @@
+package com.dsajavarev;
+
+import java.util.HashMap;
+
+public class SecondMostString {
+    public static void main(String[] args) {
+        int N = 6;
+        String[] arr = {"aaa", "bbb", "ccc", "bbb", "aaa", "aaa"};
+        System.out.println(secFrequent(arr,N));
+    }
+    static String secFrequent(String[] arr, int N)
+    {
+        // your code here
+        HashMap<String,Integer> map = new HashMap<>();
+
+        for (String s : arr) {
+            if (map.containsKey(s)) {
+                int of = map.get(s);
+                map.put(s, of + 1);
+            } else {
+                map.put(s, 1);
+            }
+        }
+        int max = 0;
+        for(String a : map.keySet()){
+            if(map.get(a) > max)
+                max = map.get(a);
+        }
+        int sec = 0;
+        String str = "";
+        for(String a : map.keySet()){
+            if(map.get(a) < max && map.get(a) > sec){
+                sec = map.get(a);
+                str = a;
+            }
+        }
+        return str;
+    }
+}
