f75f69a15eb08d7dad64b9e7b7e8f2e74c2e287a
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-10 13:15:37 +0530

    Added on 10th April 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/ConvertToLowerCase.java
@@ -0,0 +1,13 @@
+package com.dsajavarev;
+//https://practice.geeksforgeeks.org/problems/java-convert-string-to-lowercase2313/1/?page=1&difficulty[]=-2&difficulty[]=-1&difficulty[]=0&status[]=unsolved&category[]=Strings&sortBy=submissions
+public class ConvertToLowerCase {
+    public static void main(String[] args) {
+        String str = "RisSDvADa";
+        System.out.println(toLower(str));
+    }
+    static String toLower(String S) {
+        // code here
+        return S.toLowerCase();
+
+    }
+}
