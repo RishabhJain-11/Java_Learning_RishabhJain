9adb7b2ee26134209f7cf469b8b4151b4e635622
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-12 13:10:41 +0530

    Added on 12th April 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/GameOfLife.java
@@ -0,0 +1,45 @@
+package com.dsajavarev;
+//  https://leetcode.com/problems/game-of-life/
+public class GameOfLife {
+    public static void main(String[] args) {
+        int[][] board = {{0,1,0},{0,0,1},{1,1,1},{0,0,0}};
+        System.out.println(gameOfLife(board));
+    }
+    public static boolean gameOfLife(int[][] board) {
+        if (board == null || board.length == 0) return false;
+        int m = board.length, n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int lives = liveNeighbors(board, m, n, i, j);
+
+                // In the beginning, every 2nd bit is 0;
+                // So we only need to care about when will the 2nd bit become 1.
+                if (board[i][j] == 1 && lives >= 2 && lives <= 3) {
+                    board[i][j] = 3; // Make the 2nd bit 1: 01 ---> 11
+                }
+                if (board[i][j] == 0 && lives == 3) {
+                    board[i][j] = 2; // Make the 2nd bit 1: 00 ---> 10
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] >>= 1;  // Get the 2nd state.
+            }
+        }
+        return false;
+    }
+
+    public static int liveNeighbors(int[][] board, int m, int n, int i, int j) {
+        int lives = 0;
+        for (int x = Math.max(i - 1, 0); x <= Math.min(i + 1, m - 1); x++) {
+            for (int y = Math.max(j - 1, 0); y <= Math.min(j + 1, n - 1); y++) {
+                lives += board[x][y] & 1;
+            }
+        }
+        lives -= board[i][j] & 1;
+        return lives;
+    }
+}
