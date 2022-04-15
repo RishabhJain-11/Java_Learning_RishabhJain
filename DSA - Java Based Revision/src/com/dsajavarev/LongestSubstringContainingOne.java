156f21944cd19a6238436e8ab1d6c1a30432354e
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-15 10:07:14 +0530

    Added on 15th April 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

--- DSA - Java Based Revision/src/com/dsajavarev/LongestSubstringContainingOne.java
+++ DSA - Java Based Revision/src/com/dsajavarev/LongestSubstringContainingOne.java
@@ -1,5 +1,5 @@
 package com.dsajavarev;
-
+//https://practice.geeksforgeeks.org/problems/longest-substring-containing-1/1/?page=1&difficulty[]=-1&status[]=unsolved&company[]=Amazon&company[]=Microsoft&company[]=Flipkart&company[]=Adobe&company[]=Google&company[]=Zoho&company[]=Paytm&company[]=Morgan%20Stanley&company[]=Goldman%20Sachs&company[]=Walmart&company[]=D-E-Shaw&company[]=Ola%20Cabs&company[]=VMWare&company[]=Qualcomm&company[]=Intuit%20&company[]=Directi&company[]=Linkedin&company[]=Salesforce&company[]=Codenation&company[]=PayPal&company[]=Nagarro%20&company[]=GE&company[]=Media.net%20&category[]=Strings&sortBy=submissions#
 public class LongestSubstringContainingOne {
     public static void main(String[] args) {
         String s = "01110221211111134296063509";
@@ -7,8 +7,6 @@ public class LongestSubstringContainingOne {
     }
     public static int count(String s)
     {
-        //add code here.
-
         int max = 0,count = 0,len = s.length();
         for(int i = 0;i < len;i++){
             if(s.charAt(i) == '1'){
