707d3c2ef257683848cd1ee3e93389645bfd1026
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-17 09:31:39 +0530

    Added on 17th April 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/IncreasingOrderSearchTree.java
@@ -0,0 +1,30 @@
+package com.dsajavarev;
+
+public class IncreasingOrderSearchTree {
+    public static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public static void main(String[] args) {
+
+    }
+    public static TreeNode increasingBST(TreeNode root) {
+        return increasingBST(root, null);
+    }
+
+    public static TreeNode increasingBST(TreeNode root, TreeNode tail) {
+        if (root == null) return tail;
+        TreeNode res = increasingBST(root.left, root);
+        root.left = null;
+        root.right = increasingBST(root.right, tail);
+        return res;
+    }
+}
