db83e5513677f52958f92af011757fb68d0a762e
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-18 15:43:32 +0530

    Added on 18th April 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/KthSmallestElementInABST.java
@@ -0,0 +1,35 @@
+package com.dsajavarev;
+
+public class KthSmallestElementInABST {
+    public static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public static void main(String[] args) {
+
+    }
+    public static int kthSmallest(TreeNode root, int k) {
+        int count = countNodes(root.left);
+        if (k <= count) {
+            return kthSmallest(root.left, k);
+        } else if (k > count + 1) {
+            return kthSmallest(root.right, k-1-count); // 1 is counted as current node
+        }
+
+        return root.val;
+    }
+
+    public static int countNodes(TreeNode n) {
+        if (n == null) return 0;
+
+        return 1 + countNodes(n.left) + countNodes(n.right);
+    }
+}
