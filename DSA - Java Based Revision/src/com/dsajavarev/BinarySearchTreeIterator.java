ae4c7fe6757badca4d4b662fb7dd1f661b9430c7
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-20 15:28:19 +0530

    Added on 20th April 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/BinarySearchTreeIterator.java
@@ -0,0 +1,41 @@
+package com.dsajavarev;
+//https://leetcode.com/problems/binary-search-tree-iterator/submissions/
+public class BinarySearchTreeIterator {
+//   public static class TreeNode {
+//      int val;
+//      TreeNode left;
+//      TreeNode right;
+//      TreeNode() {}
+//      TreeNode(int val) { this.val = val; }
+//      TreeNode(int val, TreeNode left, TreeNode right) {
+//          this.val = val;
+//          this.left = left;
+//          this.right = right;
+//      }
+////  }
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    static Stack<TreeNode> stack = new Stack<TreeNode>();
+//
+//    public static BSTIterator(TreeNode root) {
+//        pushAll(root);
+//    }
+//
+//    /** @return whether we have a next smallest number */
+//    public static boolean hasNext() {
+//        return !stack.isEmpty();
+//    }
+//
+//    /** @return the next smallest number */
+//    public static int next() {
+//        TreeNode tmpNode = stack.pop();
+//        pushAll(tmpNode.right);
+//        return tmpNode.val;
+//    }
+//
+//    private static void pushAll(TreeNode node) {
+//        for (; node != null; stack.push(node), node = node.left);
+//    }
+}
