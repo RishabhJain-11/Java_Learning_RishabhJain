116e307437c2d1baf62b3906edca72d29b070a2b
Rishabh Jain <jainrishabh71011@gmail.com>
2022-04-24 09:50:45 +0530

    Added on 24th April 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/DesignUnderGroundSystem.java
@@ -0,0 +1,31 @@
+package com.dsajavarev;
+//https://leetcode.com/problems/design-underground-system/
+import java.util.HashMap;
+
+public class DesignUnderGroundSystem {
+    public static void main(String[] args) {
+
+//    }
+//    static HashMap<String, Pair<Integer, Integer>> checkoutMap = new HashMap<String, MinimumCostPath.Pair<Integer, Integer>>(); // Route - {TotalTime, Count}
+//    static HashMap<Integer, Pair<String, Integer>> checkInMap = new HashMap<Integer, Pair<String, Integer>>();  // Uid - {StationName, Time}
+//
+//    public UndergroundSystem() {}
+//
+//    public void checkIn(int id, String stationName, int t) {
+//        checkInMap.put(id, new Pair<>(stationName, t));
+//    }
+//
+//    public void checkOut(int id, String stationName, int t) {
+//        Pair<String, Integer> checkIn = checkInMap.get(id);
+//        String route = checkIn.getKey() + "_" + stationName;
+//        int totalTime = t - checkIn.getValue();
+//        Pair<Integer, Integer> checkout = checkoutMap.getOrDefault(route, new Pair<>(0, 0));
+//        checkoutMap.put(route, new Pair<>(checkout.getKey() + totalTime, checkout.getValue() + 1));
+//    }
+//
+//    public double getAverageTime(String startStation, String endStation) {
+//        String route = startStation + "_" + endStation;
+//        Pair<Integer, Integer> checkout = checkoutMap.get(route);
+//        return (double) checkout.getKey() / checkout.getValue();
+    }
+}
