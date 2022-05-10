f2d51382e7ed179e811094af23cf2ef6e15d2c91
Rishabh Jain <jainrishabh71011@gmail.com>
2022-05-10 23:21:12 +0530

    Added on 10th May 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/CombinationSumThree.java
@@ -0,0 +1,28 @@
+package com.dsajavarev;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSumThree {
+    public static void main(String[] args) {
+        int k = 3,n=7;
+        System.out.println(combinationSum3(k,n));
+    }
+    public static List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        combination(ans,new ArrayList<Integer>(),k,1,n);
+        return ans;
+    }
+    private static void combination(List<List<Integer>> ans,List<Integer> comb,int k,int start,int n){
+        if(comb.size() == k && n == 0){
+            List<Integer> li = new ArrayList<Integer>(comb);
+            ans.add(li);
+            return;
+        }
+        for(int i = start;i <= 9;i++){
+            comb.add(i);
+            combination(ans,comb,k,i+1,n-i);
+            comb.remove(comb.size() - 1);
+        }
+    }
+}
