09a43ee538db6e23df82da2946b4aa9eaa978043
Rishabh Jain <jainrishabh71011@gmail.com>
2022-05-11 23:39:13 +0530

    Added on 11th May 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/CountStoredVowelStrings.java
@@ -0,0 +1,15 @@
+package com.dsajavarev;
+
+public class CountStoredVowelStrings {
+    public static void main(String[] args) {
+        int n = 1;
+        System.out.println(countVowelStrings(n));
+    }
+    public static int countVowelStrings(int n) {
+        int[][] dp = new int[n + 1][6];
+        for (int i = 1; i <= n; ++i)
+            for (int k = 1; k <= 5; ++k)
+                dp[i][k] = dp[i][k - 1] + (i > 1 ? dp[i - 1][k] : 1);
+        return dp[n][5];
+    }
+}
