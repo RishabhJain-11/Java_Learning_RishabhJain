eab89d35d79e667e2e951820e765f624694fd203
Rishabh Jain <jainrishabh71011@gmail.com>
2022-05-25 13:09:26 +0530

    Added on 25th May 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/RussianDollEnvelopes.java
@@ -0,0 +1,35 @@
+package com.dsajavarev;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class RussianDollEnvelopes {
+    public static void main(String[] args) {
+        int[][] envelopes = {{5,4},{6,4},{6,7},{2,3}};
+        System.out.println(maxEnvelopes(envelopes));
+    }
+    public static int maxEnvelopes(int[][] envelopes) {
+        if(envelopes == null || envelopes.length == 0
+                || envelopes[0] == null || envelopes[0].length != 2)
+            return 0;
+        Arrays.sort(envelopes, new Comparator<int[]>(){
+            public int compare(int[] arr1, int[] arr2){
+                if(arr1[0] == arr2[0])
+                    return arr2[1] - arr1[1];
+                else
+                    return arr1[0] - arr2[0];
+            }
+        });
+        int dp[] = new int[envelopes.length];
+        int len = 0;
+        for(int[] envelope : envelopes){
+            int index = Arrays.binarySearch(dp, 0, len, envelope[1]);
+            if(index < 0)
+                index = -(index + 1);
+            dp[index] = envelope[1];
+            if(index == len)
+                len++;
+        }
+        return len;
+    }
+}
