06e82af61eece02f73579f5dd953dbd4e8a0ff4f
Rishabh Jain <jainrishabh71011@gmail.com>
2022-05-28 20:04:25 +0530

    Added on 28th May 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

--- DSA - Java Based Revision/src/com/dsajavarev/MissingNumber.java
+++ DSA - Java Based Revision/src/com/dsajavarev/MissingNumber.java
@@ -2,14 +2,13 @@ package com.dsajavarev;
 
 public class MissingNumber {
     public static void main(String[] args) {
-        int nums[] = {3,0,1};
+        int[] nums = {3,0,1};
         System.out.println(missingNumber(nums));
     }
     public static int missingNumber(int[] nums) {
         int n = nums.length;
         int result = n * (n + 1)/2;
-        for(int i=0;i<nums.length;i++)
-            result -= nums[i];
+        for (int num : nums) result -= num;
         return result;
     }
 }
