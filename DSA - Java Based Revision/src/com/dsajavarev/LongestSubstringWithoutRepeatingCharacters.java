c36a9e6211f4546163d38b43b2a4940f39aa9a38
Rishabh Jain <jainrishabh71011@gmail.com>
2022-06-10 11:22:34 +0530

    Added on 10th June 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,26 @@
+package com.dsajavarev;
+
+import java.util.HashMap;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+    public static int lengthOfLongestSubstring(String s){
+        if(s.length() == 0)
+            return 0;
+
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max = 0;
+
+        for (int i = 0,j = 0;i < s.length();i++){
+            if (map.containsKey(s.charAt(i))){
+                j = Math.max(j,map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i),i);
+            max = Math.max(max,i - j + 1);
+        }
+        return max;
+    }
+}
