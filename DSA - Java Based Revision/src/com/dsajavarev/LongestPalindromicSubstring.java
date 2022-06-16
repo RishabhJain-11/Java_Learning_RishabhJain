cbcd9cfb3513b879c12d474d4314b4f79df96c71
Rishabh Jain <jainrishabh71011@gmail.com>
2022-06-16 22:30:17 +0530

    Added on 16th June 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/LongestPalindromicSubstring.java
@@ -0,0 +1,33 @@
+package com.dsajavarev;
+
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        String s = "babad";
+        System.out.println(longestPalindrome(s));
+
+    }
+    private static int lo, maxLen;
+
+    public static String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2)
+            return s;
+
+        for (int i = 0; i < len-1; i++) {
+            extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
+            extendPalindrome(s, i, i+1); //assume even length.
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    private static void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }
+}
