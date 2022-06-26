2d3fafcde9f0b8874874c6c984d52d72ee384420
Rishabh Jain <jainrishabh71011@gmail.com>
2022-06-26 15:09:25 +0530

    Added on 26th June 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/MaximumPointsYouCanObtainFromCards.java
@@ -0,0 +1,25 @@
+package com.dsajavarev;
+
+public class MaximumPointsYouCanObtainFromCards {
+    public static void main(String[] args) {
+        int[] cardPoints = {1,2,3,4,5,6,1};
+        int k = 3;
+        System.out.println(maxScore(cardPoints,k));
+    }
+    public static int maxScore(int[] cardPoints, int k) {
+        int[] dp = new int[k + 1];
+        //   this dp array stores the total points when taking i cards from left.
+        //   since we could take 0 - k cards from left ,the length of the dp array would be k+1.
+
+        for (int i = cardPoints.length - 1; i >= cardPoints.length - k; i--) {
+            dp[0] += cardPoints[i];
+        }
+        int max_points = dp[0];
+
+        for (int i = 1; i < k + 1; i++) {
+            dp[i] = dp[i - 1] + cardPoints[i - 1] - cardPoints[cardPoints.length - k + i - 1];
+            max_points = Math.max(max_points, dp[i]);
+        }
+        return max_points;
+    }
+}
