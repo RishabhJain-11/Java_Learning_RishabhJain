d37aa341cdb51d7be816609e237c38773be5bc4a
Rishabh Jain <jainrishabh71011@gmail.com>
2022-06-29 15:15:52 +0530

    Added on 29th June 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/QueueReconstructionByHeight.java
@@ -0,0 +1,36 @@
+package com.dsajavarev;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class QueueReconstructionByHeight {
+    public static void main(String[] args) {
+        int[][] people = {{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
+        System.out.println(Arrays.deepToString(reconstructQueue(people)));
+    }
+    public static int[][] reconstructQueue(int[][] people) {
+        if (people == null || people.length == 0 || people[0].length == 0)
+            return new int[0][0];
+
+        Arrays.sort(people, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                if (b[0] == a[0]) return a[1] - b[1];
+                return b[0] - a[0];
+            }
+        });
+
+        int n = people.length;
+        ArrayList<int[]> tmp = new ArrayList<>();
+        for (int[] person : people) tmp.add(person[1], new int[]{person[0], person[1]});
+
+        int[][] res = new int[people.length][2];
+        int i = 0;
+        for (int[] k : tmp) {
+            res[i][0] = k[0];
+            res[i++][1] = k[1];
+        }
+
+        return res;
+    }
+}
