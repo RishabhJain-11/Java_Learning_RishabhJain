ec3cdabe834d79b7e480b38cd77b315f1e245b25
Rishabh Jain <jainrishabh71011@gmail.com>
2022-07-04 20:58:39 +0530

    Added on 04th July 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/Candy.java
@@ -0,0 +1,27 @@
+package com.dsajavarev;
+
+import java.util.Arrays;
+
+public class Candy {
+    public static void main(String[] args) {
+        int[] ratings = {1,0,2};
+        System.out.println(candy(ratings));
+    }
+    public static int candy(int[] ratings) {
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);// Give each child 1 candy
+
+        for (int i = 1; i < candies.length; i++){// Scan from left to right, to make sure right higher rated child gets 1 more candy than left lower rated child
+            if (ratings[i] > ratings[i - 1]) candies[i] = (candies[i - 1] + 1);
+        }
+
+        for (int i = candies.length - 2; i >= 0; i--) {// Scan from right to left, to make sure left higher rated child gets 1 more candy than right lower rated child
+            if (ratings[i] > ratings[i + 1]) candies[i] = Math.max(candies[i], (candies[i + 1] + 1));
+        }
+
+        int sum = 0;
+        for (int candy : candies)
+            sum += candy;
+        return sum;
+    }
+}
