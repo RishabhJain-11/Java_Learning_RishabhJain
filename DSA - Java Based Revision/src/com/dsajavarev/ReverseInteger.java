a466cddf942e5f06cd0dee059ac2453cbce6c8a3
Rishabh Jain <jainrishabh71011@gmail.com>
2022-07-08 22:15:32 +0530

    Added on 08th July 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/ReverseInteger.java
@@ -0,0 +1,18 @@
+package com.dsajavarev;
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+        int x = 123;
+        System.out.println(reverse(x));
+    }
+    public static int reverse(int x) {
+        long rev = 0;
+        while(x != 0){
+            rev = rev * 10 + x % 10;
+            x = x/10;
+            if(rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE)
+                return 0;
+        }
+        return (int) rev;
+    }
+}
