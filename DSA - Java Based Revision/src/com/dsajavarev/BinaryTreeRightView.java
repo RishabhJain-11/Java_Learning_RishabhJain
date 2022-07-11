165d89e7de1c34b3faa5fe7a244cb842828b205d
Rishabh Jain <jainrishabh71011@gmail.com>
2022-07-11 15:38:59 +0530

    Added on 11th July 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/BinaryTreeRightView.java
@@ -0,0 +1,55 @@
+package com.dsajavarev;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+class Node {
+    int key;
+    Node left = null,right = null;
+
+    Node(int key){
+        this.key = key;
+    }
+}
+public class BinaryTreeRightView {
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.right = new Node(4);
+        root.right.left = new Node(5);
+        root.right.right = new Node(6);
+        root.right.left.left = new Node(7);
+        root.right.left.right = new Node(8);
+
+        printRightView(root);
+    }
+    public static void printRightView(Node root){
+        if(root == null)
+            return;
+
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        Node curr = null;
+
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            int i = 0;
+
+            while (i++ < size){
+                curr = queue.poll();
+
+                if(i == size){
+                    System.out.print(curr.key + " ");
+                }
+                if(curr.left != null){
+                    queue.add(curr.left);
+                }
+                if (curr.right != null){
+                    queue.add(curr.right);
+                }
+            }
+        }
+    }
+}
--- DSA - Java Based Revision/src/com/dsajavarev/KthSmallestElement.java
+++ DSA - Java Based Revision/src/com/dsajavarev/KthSmallestElement.java
@@ -7,12 +7,47 @@ public class KthSmallestElement {
         int N = 6;
         int[] arr = {7 ,10 ,4 ,3 ,20 ,15};
         int K = 3;
-//        System.out.println(kthSmallest(arr,N,K));
+        int l = 0;
+        int r = arr.length;
+        System.out.println(kthSmallest(arr,l,r,K));
     }
+//    public static int kthSmallest(int[] arr, int l, int r, int k)
+//    {
+//        //Your code here
+////        Arrays.sort(arr);
+////        return arr[k-1];
+//
+//
+//
+//
+//    }
+
     public static int kthSmallest(int[] arr, int l, int r, int k)
     {
         //Your code here
-        Arrays.sort(arr);
-        return arr[k-1];
+        int partition = partition(arr,l,r);
+        if(partition == k-1)
+            return arr[partition];
+        else if(partition < k - 1)
+            return kthSmallest(arr,partition+1,r,k);
+        else
+            return kthSmallest(arr,l,partition-1,k);
+
+    }
+    public static int partition(int[] arr, int low, int high){
+        int pivot = arr[high], pivotloc = low;
+        for(int i=low;i<=high;i++){
+            if(arr[i] < pivot){
+                int temp = arr[i];
+                arr[i] = arr[pivotloc];
+                arr[pivotloc] = temp;
+                pivotloc++;
+            }
+        }
+        int temp = arr[high];
+        arr[high] = arr[pivotloc];
+        arr[pivotloc] = temp;
+
+        return pivotloc;
     }
 }
