edfee7faee72caf326c0ed30af0ae26468936842
Rishabh Jain <jainrishabh71011@gmail.com>
2022-08-04 20:01:36 +0530

    Added on 04th August 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

---
+++ DSA - Java Based Revision/src/com/dsajavarev/MirrorReflection.java
@@ -0,0 +1,25 @@
+package com.dsajavarev;
+
+public class MirrorReflection {
+    public static void main(String[] args) {
+        int p = 2;
+        int q = 1;
+        System.out.println(mirrorReflection(p,q));
+    }
+    public static int mirrorReflection(int p, int q) {
+        int m = 1, n = 1;
+        while(m * p != n * q){
+            n++;
+            m = n * q / p;
+        }
+
+        if(m % 2 == 0 && n % 2 == 1)
+            return 0;
+        if(m % 2 == 1 && n % 2 == 1)
+            return 1;
+        if(m % 2 == 1 && n % 2 == 0)
+            return 2;
+
+        return -1;
+    }
+}
