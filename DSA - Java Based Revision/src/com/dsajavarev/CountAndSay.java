037d0f531f87839b780f86032bd9391d1cdbc04d
Rishabh Jain <jainrishabh71011@gmail.com>
2022-10-10 20:58:17 +0530

    Added on 10th October 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

--- DSA - Java Based Revision/src/com/dsajavarev/CountAndSay.java
+++ DSA - Java Based Revision/src/com/dsajavarev/CountAndSay.java
@@ -2,9 +2,35 @@ package com.dsajavarev;
 
 public class CountAndSay {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 10;
         System.out.println(countAndSay(n));
     }
+
+    public static String countAndSay(int n) {
+        StringBuilder curr = new StringBuilder("1");
+        StringBuilder prev;
+        int count;
+        char say;
+
+        for (int i = 1; i < n; i++) {
+            prev = curr;
+            curr = new StringBuilder();
+            count = 1;
+            say = prev.charAt(0);
+
+            for (int j = 1, len = prev.length(); j < len; j++) {
+                if (prev.charAt(j) != say) {
+                    curr.append(count).append(say);
+                    count = 1;
+                    say = prev.charAt(j);
+                } else count++;
+            }
+            curr.append(count).append(say);
+        }
+        return curr.toString();
+    }
+}
+    /*
     public static String countAndSay(int n){
         String s = "1";
         for (int i = 1; i < n; i++) {
@@ -32,4 +58,4 @@ public class CountAndSay {
         sb.append(c);
         return sb.toString();
     }
-}
+     */
