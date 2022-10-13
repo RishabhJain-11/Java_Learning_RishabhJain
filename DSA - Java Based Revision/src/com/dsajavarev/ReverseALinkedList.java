d22b086a8a40459c0213fa4f2bbbec01280d318c
Rishabh Jain <jainrishabh71011@gmail.com>
2022-10-13 10:55:27 +0530

    Added on 13th October 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

--- DSA - Java Based Revision/src/com/dsajavarev/ReverseALinkedList.java
+++ DSA - Java Based Revision/src/com/dsajavarev/ReverseALinkedList.java
@@ -12,15 +12,30 @@ public class ReverseALinkedList {
   }
     public static void main(String[] args) {
 //
+//        CountNodesInLinkedList head = new CountNodesInLinkedList();
+//        head.()
+//        System.out.println(reverseList(head));
     }
-    public ListNode reverseList(ListNode head){
-        if(head == null || head.next == null)
-            return head;
+//    public ListNode reverseList(ListNode head){
+//        if(head == null || head.next == null)
+//            return head;
+//
+//        ListNode root = reverseList(head.next);
+//
+//        head.next.next = head;
+//        head.next = null;
+//        return root;
+//    }
+    public static ListNode reverseList(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
 
-        ListNode root = reverseList(head.next);
-
-        head.next.next = head;
-        head.next = null;
-        return root;
+        while (curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 }
