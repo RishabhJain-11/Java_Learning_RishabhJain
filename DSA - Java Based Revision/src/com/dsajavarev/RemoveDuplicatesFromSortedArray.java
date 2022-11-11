57e713de7cd57e081596b0a8b369dddfe0a44da9
Rishabh Jain <jainrishabh71011@gmail.com>
2022-11-11 19:05:40 +0530

    Added on 11th November 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

--- DSA - Java Based Revision/src/com/dsajavarev/RemoveDuplicatesFromSortedArray.java
+++ DSA - Java Based Revision/src/com/dsajavarev/RemoveDuplicatesFromSortedArray.java
@@ -5,6 +5,7 @@ public class RemoveDuplicatesFromSortedArray {
         int[] nums = {0,0,1,1,1,2,2,3,3,4};
         System.out.println(removeDuplicates(nums));
     }
+
     public static int removeDuplicates(int[] nums){
         int i = 0;
         for(int n : nums){
