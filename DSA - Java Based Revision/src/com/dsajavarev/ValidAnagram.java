742b1f1d0039f44586f732b9e3f8b0fc6b167939
Rishabh Jain <jainrishabh71011@gmail.com>
2022-11-30 22:43:04 +0530

    Added on 30th November 2022
    Signed-off-by: Rishabh Jain <jainrishabh71011@gmail.com>

--- DSA - Java Based Revision/src/com/dsajavarev/ValidAnagram.java
+++ DSA - Java Based Revision/src/com/dsajavarev/ValidAnagram.java
@@ -5,23 +5,44 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ValidAnagram {
+//    public static void main(String[] args) {
+//        String s = "anagram", t = "nagaram";
+//        System.out.println(valid(s,t));
+//    }
+//    public static boolean valid(String s,String t){
+//        /*
+//        if(s == null || t == null)
+//            return s == t;
+//
+//        char[] sChar = s.toCharArray();
+//        char[] tChar = t.toCharArray();
+//
+//        Arrays.sort(sChar);
+//        Arrays.sort(tChar);
+//        return Arrays.equals(sChar,tChar);
+//         */
+//
+//        if(s.length() != t.length()){
+//            return false;
+//        }
+//
+//        Map<Character, Integer> maps = new HashMap<>();
+//        for(char c : s.toCharArray()){
+//            maps.put(c, maps.getOrDefault(c, 0) + 1);
+//        }
+//        Map<Character, Integer> mapt = new HashMap<>();
+//        for(char c : t.toCharArray()){
+//            mapt.put(c, mapt.getOrDefault(c, 0) + 1);
+//        }
+//
+//        return maps.equals(mapt);
+//     }
+
     public static void main(String[] args) {
         String s = "anagram", t = "nagaram";
-        System.out.println(valid(s,t));
+        System.out.println(isAnagram(s,t));
     }
-    public static boolean valid(String s,String t){
-        /*
-        if(s == null || t == null)
-            return s == t;
-
-        char[] sChar = s.toCharArray();
-        char[] tChar = t.toCharArray();
-
-        Arrays.sort(sChar);
-        Arrays.sort(tChar);
-        return Arrays.equals(sChar,tChar);
-         */
-
+    public static boolean isAnagram(String s, String t){
         if(s.length() != t.length()){
             return false;
         }
@@ -36,5 +57,5 @@ public class ValidAnagram {
         }
 
         return maps.equals(mapt);
-     }
+    }
 }
